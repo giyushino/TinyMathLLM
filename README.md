587675cf41f992fc8f0e634cbbac9104e69a85de
giyushino <allanzhang440@gmail.com>
2025-01-25 14:39:32 -0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # TinyMathLLM
-Testing different strategies to finetune small LLMs to be effective math chatbots
+
+## Purpose
+This project aims to determine whether or not a large language model with "only" 1.1 billion parameters can learn math with minimal compute power. LLMs have become increasingly proficient in all fields, including math. With improvements in chain of thought, training strategies, etc, LLMs are now more capable than ever, but they are also becoming increasingly harder to run locally as they naturally grow larger in size.
+
+The idea I would like to test with this repo is if small, lightweight models can become capable math chatbots. Currently, my hypothesis is yes, but I believe that model would need to act more as an intermediary between the user and a math API. Rather than having the model attempt to laern to solve all kinds of different problems, I believe it would be much easier for the model to learn to extract information from user inputs, determine what kind of question it is, and then use the proper tools to solve accordingly. 
+
+Currently, I'm only testing the model on questions regarding addition, multiplication, derivatives, integration, matrix multiplication, and determinants. Using 4-bit quantization and low rank adaptation, I'm making training and inference extremely light, though I believe this is having a non-neglible impact on model perfomance. Once I get access to more compute power, I will train the model on answering questions + extracting information/classification again. 
+
+
+
+
--- src/eval/deepseek_eval.py
+++ src/eval/deepseek_eval.py
@@ -9,16 +9,49 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 #*******************************
 
+def deepseek_chat():
+    """
+    probably self explantory
+    """
+    
+    count = -1
+    client = OpenAI(api_key=OPENAI_API_KEY, base_url="https://api.deepseek.com") 
+    
+    with open(r"C:\Users\allan\nvim\tinyMath\TinyMathLLM\src\eval\base_llama\answer.json", "r", errors = "ignore") as model_output:
+        with open(r"C:\Users\allan\nvim\tinyMath\TinyMathLLM\src\deepseek_judge\answer\deepseek_answer.json", "a") as deepseek_eval:
+            for line in model_output:
+                temp = {}
+                count += 1
+                if count % 2 == 0:
+                    data = json.loads(line.strip())
+                    question = data["input"][1]
+                    answer = data["output"]
+                
+                    messages = [
+                        {
+                            "role": "system",
+                            "content": """Provided with the the following question and answer, first, identify what kind of question it is from this list: ["Addition", "Multiplication", "Derivation", "Integration", "MatrixMultiplication", "Determinant"]. Then respond if answer is correct. Your reponse should look like 'Addition: NO'. Here is the question: {}. Answer: {}. DO NOT PROVIDE AN EXPLANATION. YOUR ANSWER SHOULD LOOK ONLY BE 2 WORDS.""".format(question, answer)
+                        }
+                    ]
+
+                    messages.append({"role": "user", "content": "Question: {}. Answer: {}".format(question, answer)})
+                    response = client.chat.completions.create(
+                         model="deepseek-chat",
+                         messages=messages,
+                         max_tokens=1024,
+                         temperature=0.7,
+                         stream=False
+                    )
+                    bot_reply = response.choices[0].message.content
+                    print(bot_reply, count)
+                    temp["Question"] = question["content"]
+                    temp["Judgement"] = bot_reply
+                    json_line = json.dumps(temp)
+                    deepseek_eval.write(json_line + "\n")
+     
+deepseek_chat()
 
 
-
-
-#pipeline = load_model_pipeline("TinyLlama/TinyLlama-1.1B-Chat-v1.0", role = "You are a mathematics professor who helps students with their math problems. Provide them with the answer")
-
-
-llama, llama_tokenizer = load_quantized_model("TinyLlama/TinyLlama-1.1B-Chat-v1.0")
-get_quantized_response(llama, llama_tokenizer, role = "You are a mathematics professor who helps students with their math problems. Provide them with the answer")
- 
 
 
 
